f1e0ea032d984abec7c636a229fb3907d6f0ae0d
oubi-aed <alexander.oberhoer23@gmail.com>
2025-02-18 17:01:54 +0100

    entfernen des session states zum speichern

--- ui_main.py
+++ ui_main.py
@@ -5,55 +5,52 @@
 import streamlit as st
 from tinydb import TinyDB, Query
 import matplotlib.pyplot as plt
-
+from Gelenk import Gelenk
 
 # Konfigurieren der Streamlit-Seite
 st.set_page_config(layout="wide")
 
+button_neues_Gelenk = False
+
 # Unterteilung in zwei Spalten
 eingabe, ausgabe = st.columns(2)
-
-
-# Initialisieren der Variablen im Session State
-if 'show_joint_inputs' not in st.session_state:
-    st.session_state.show_joint_inputs = False
-if 'x_start' not in st.session_state:
-    st.session_state.x_start = ""
-if 'y_start' not in st.session_state:
-    st.session_state.y_start = ""
-if 'x_end' not in st.session_state:
-    st.session_state.x_end = ""
-if 'y_end' not in st.session_state:
-    st.session_state.y_end = ""
-if 'statisch' not in st.session_state:
-    st.session_state.statisch = False
-if 'statisch2' not in st.session_state:
-    st.session_state.statisch = False
 
 
 with eingabe:
     st.header("Eingabe")
     # Button zum Anzeigen der Eingabefelder für ein neues Gelenk
     if st.button("neues Gelenk"):
-        st.session_state.show_joint_inputs = True
+        button_neues_Gelenk = True
 
     # Eingabefelder anzeigen, wenn der Button gedrückt wurde
-    if st.session_state.show_joint_inputs:
+    if button_neues_Gelenk:
         start1, start2 = st.columns(2)
 
+    #Startpunkte definieren
         with start1:
-            st.session_state.x_start = st.text_input("x-Koordinate Start", st.session_state.x_start)
+            x_start = st.text_input("x-Koordinate Start", "hier Wert eingeben")
         with start2:
-            st.session_state.y_start = st.text_input("y-Koordinate Start", st.session_state.y_start)
-        statisch = st.checkbox("Statisch", st.session_state.statisch)
+            y_start = st.text_input("y-Koordinate Start", "hier Wert eingeben")
+        statisch = st.checkbox("Statisch", False)
+
+    # Button zum Speichern des Gelenks
+        gelenk_gespeichert = st.form_submit_button("Gelenk speichern")
+        if gelenk_gespeichert:
+            neues_gelenk = Gelenk(x_start, y_start, x_end, y_end, statisch, statisch2)
+            neues_gelenk.speichern()
+            st.write("Gelenk gespeichert")
 
 
+    #Endpunkte definieren
         end1, end2 = st.columns(2)
         with end1:
-            st.session_state.x_end = st.text_input("x-Koordinate Ende", st.session_state.x_end)
+            x_end = st.text_input("x-Koordinate Ende", "hier Wert eingeben")
         with end2:
-            st.session_state.y_end = st.text_input("y-Koordinate Ende", st.session_state.y_end)
-        statisch2 = st.checkbox("Statisch2", st.session_state.statisch)
+            y_end = st.text_input("y-Koordinate Ende", "hier Wert eingeben")
+        statisch2 = st.checkbox("Statisch2", False)
+
+        
+
 
 with ausgabe:
     st.header("Simulation")
@@ -65,11 +62,11 @@ with ausgabe:
 
 
     # Visualisieren der Gelenke und Glieder
-    if st.session_state.x_start and st.session_state.y_start:
-        ax.scatter(st.session_state.x_start, st.session_state.y_start, color="red")
+    if x_start and y_start:
+        ax.scatter(x_start, y_start, color="red")
 
     if st.session_state.x_end and st.session_state.y_end:
-        ax.scatter(st.session_state.x_end, st.session_state.y_end, color="blue")
+        ax.scatter(x_end, y_end, color="blue")
     st.pyplot(fig)
 
 st.write("Session State:")
